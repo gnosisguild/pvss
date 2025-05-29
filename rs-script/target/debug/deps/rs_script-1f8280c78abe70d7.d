0f8c705c02959eacef3dd01a1ce2274bac03ba11
Zara Javar <zara@gnosisguild.org>
2025-05-29 10:13:50 -0700

    Remove rs-script/target/

--- rs-script/target/debug/deps/rs_script-1f8280c78abe70d7.d
+++
@@ -1,5 +0,0 @@
-/Users/zahrajavar/Documents/gnosis/code/pvss/rs-script/target/debug/deps/librs_script-1f8280c78abe70d7.rmeta: src/main.rs
-
-/Users/zahrajavar/Documents/gnosis/code/pvss/rs-script/target/debug/deps/rs_script-1f8280c78abe70d7.d: src/main.rs
-
-src/main.rs:
