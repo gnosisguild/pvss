878bd4e8e16f3d3e8600c9cf155a29d986501488
4rmanc <ozgurarmancyigit@hotmail.com>
2025-06-23 18:07:13 +0300

    relinearization from lbfv

--- crates/public_key/src/pk.rs
+++ crates/public_key/src/pk.rs
@@ -1,7 +1,9 @@
-use fhe::bfv::{BfvParametersBuilder, Encoding, Plaintext, PublicKey, SecretKey};
+use fhe::bfv::{BfvParametersBuilder, Encoding, Plaintext, SecretKey};
+use fhe::lbfv::LBFVPublicKey;
+use fhe::lbfv::LBFVRelinearizationKey;
 use rand::prelude::*;
 
-pub fn keygen_with_library() -> (SecretKey, PublicKey) {
+pub fn keygen() -> (SecretKey, LBFVPublicKey) {
     let mut rng = thread_rng();
 
     let n: u64 = 1024;
@@ -12,19 +14,32 @@ pub fn keygen_with_library() -> (SecretKey, PublicKey) {
         .set_degree(n as usize)
         .set_plaintext_modulus(plaintext_modulus)
         .set_moduli(&moduli)
-        //.set_moduli_sizes(&moduli_sizes)
         .build_arc()
         .unwrap();
 
     let sk = SecretKey::random(&params, &mut rng);
-    let pk = PublicKey::new(&sk, &mut rng);
+    let pk = LBFVPublicKey::new(&sk, &mut rng);
 
     (sk, pk)
 }
 
+pub fn relinearization_key(sk: SecretKey, pk: LBFVPublicKey) -> LBFVRelinearizationKey {
+    let mut rng = thread_rng();
+    // Create relinearization key
+    let relin_key = LBFVRelinearizationKey::new(
+        &sk, &pk, None, // Use random d1_seed
+        0,    // ciphertext level
+        0,    // key level
+        &mut rng,
+    )
+    .unwrap();
+
+    relin_key
+}
+
 #[test]
 fn test_pk() {
-    let (sk, pk) = keygen_with_library();
+    let (sk, pk) = keygen();
 
     println!("Secret key sk: {:?}", sk);
     println!("Public key pk: {:?}", pk);
