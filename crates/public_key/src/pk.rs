290e984ae892c301f0784ead4f14a264e8e10f58
4rmanc <ozgurarmancyigit@hotmail.com>
2025-06-23 17:52:53 +0300

    use fhe pk

--- crates/public_key/src/pk.rs
+++ crates/public_key/src/pk.rs
@@ -1,46 +1,31 @@
-use num_bigint::BigInt;
-use num_traits::FromPrimitive;
-use polynomial::Polynomial;
-use rand::distributions::Uniform;
+use fhe::bfv::{BfvParametersBuilder, Encoding, Plaintext, PublicKey, SecretKey};
 use rand::prelude::*;
 
-const SIGMA: f64 = 3.2;
+pub fn keygen_with_library() -> (SecretKey, PublicKey) {
+    let mut rng = thread_rng();
 
-// TODO: should this return 19, -19 all the time?
-fn gaussian_bound() -> i64 {
-    (SIGMA * 6.0).ceil() as i64
+    let n: u64 = 1024;
+    let plaintext_modulus: u64 = 2048;
+    let moduli: Vec<u64> = vec![4503599625535489, 4503599626321921];
+
+    let params = BfvParametersBuilder::new()
+        .set_degree(n as usize)
+        .set_plaintext_modulus(plaintext_modulus)
+        .set_moduli(&moduli)
+        //.set_moduli_sizes(&moduli_sizes)
+        .build_arc()
+        .unwrap();
+
+    let sk = SecretKey::random(&params, &mut rng);
+    let pk = PublicKey::new(&sk, &mut rng);
+
+    (sk, pk)
 }
 
-fn sample_small_secret<R: Rng>(rng: &mut R, degree: usize) -> Polynomial {
-    let dist = Uniform::new_inclusive(-1, 1);
-    let coeffs: Vec<BigInt> = (0..=degree)
-        .map(|_| BigInt::from(dist.sample(rng)))
-        .collect();
-    Polynomial::new(coeffs)
-}
+#[test]
+fn test_pk() {
+    let (sk, pk) = keygen_with_library();
 
-fn sample_error<R: Rng>(rng: &mut R, degree: usize, bound: i64) -> Polynomial {
-    let dist = Uniform::new_inclusive(-bound, bound);
-    let coeffs: Vec<BigInt> = (0..=degree)
-        .map(|_| BigInt::from(dist.sample(rng)))
-        .collect();
-    Polynomial::new(coeffs)
-}
-
-fn keygen<R: Rng>(rng: &mut R, a: &Polynomial) -> (Polynomial, (Polynomial, Polynomial)) {
-    let degree = a.degree();
-    let bound = gaussian_bound();
-
-    let sk = sample_small_secret(rng, degree);
-    let e_ek = sample_error(rng, degree, bound);
-
-    let a_times_sk = a.mul(&sk);
-    let minus_a_times_sk = a_times_sk.neg();
-    let b = minus_a_times_sk.add(&e_ek);
-
-    let q = BigInt::from_i128(16777216).unwrap();
-    let b_mod_q = b.reduce_and_center(&q);
-    let a_mod_q = a.reduce_and_center(&q);
-
-    (sk, (b_mod_q, a_mod_q))
+    println!("Secret key sk: {:?}", sk);
+    println!("Public key pk: {:?}", pk);
 }
