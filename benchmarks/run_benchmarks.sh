38e7423b7e0bb99ffa5e3f28823b6f3b9962f8db
0xjei <hello@0xjei.dev>
2025-12-12 18:00:44 +0100

    initialize new repository structure and port over enc_bfv

--- benchmarks/run_benchmarks.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Convenience wrapper - forwards to scripts/run_benchmarks.sh
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-exec "${SCRIPT_DIR}/scripts/run_benchmarks.sh" "$@"
-
